f31a51782eb69897dbc0212f88f5771ef2a4ef9f
Sovanmista <susovanmishra9@gmail.com>
2025-01-21 00:58:17 +0530

    second commit

---
+++ src/components/Main.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Hero from './Hero'
+import Section2 from './Setion2'
+import Section3 from './Section3'
+function Main() {
+  return (
+    <div>
+      <Hero/>
+      <Section2/>
+    <Section3/>
+    </div>
+  )
+}
+
+export default Main
---
+++ src/components/Setion2.js
@@ -0,0 +1,77 @@
+import { Truck } from 'lucide-react';
+import { UtensilsCrossed } from 'lucide-react';
+import { Timer } from 'lucide-react';
+export default function Example() {
+    return (
+      <div className="bg-gray-50 py-24 sm:py-32">
+        <div className="mx-auto max-w-2xl px-6 lg:max-w-7xl lg:px-8">
+          <h2 className="text-center text-base/7 font-semibold text-indigo-600">Deploy faster</h2>
+          <p className="mx-auto mt-2 max-w-lg text-balance text-center text-4xl font-semibold tracking-tight text-gray-950 sm:text-5xl">
+            Everything you need to deploy your app
+          </p>
+          <div className="mt-10 grid gap-4 sm:mt-16 lg:grid-cols-3 lg:grid-rows-2">
+            <div className="relative lg:row-span-2 pb-10">
+              <div className="absolute inset-px rounded-lg bg-white lg:rounded-l-[2rem]"></div>
+              <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)] lg:rounded-l-[calc(2rem+1px)]">
+                <div className="px-8 pb-3 justify-center pt-8 sm:px-10 sm:pb-0 sm:pt-10">
+                    <div className='bg-red-300 pt-7 w-20 block mx-auto h-20 rounded-full'>
+                    <Truck className='mx-auto content-center'/>
+                    </div>
+                  
+                   
+                  <p className="mt-2 text-lg font-medium tracking-tight text-gray-950 max-lg:text-center">
+                    Mobile friendly
+                  </p>
+                  <p className="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
+                    Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.
+                  </p>
+                </div>
+                
+              </div>
+              <div className="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5 lg:rounded-l-[2rem]"></div>
+            </div>
+          
+            
+            <div className="relative lg:row-span-2">
+              <div className="absolute inset-px rounded-lg bg-white max-lg:rounded-b-[2rem] lg:rounded-r-[2rem]"></div>
+              <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)] max-lg:rounded-b-[calc(2rem+1px)] lg:rounded-r-[calc(2rem+1px)]">
+                <div className="px-8 pb-3 pt-8 sm:px-10 sm:pb-0 sm:pt-10">
+                <div className='bg-red-300 pt-7 w-20 block mx-auto h-20 rounded-full'>
+                    <UtensilsCrossed className='mx-auto content-center'/>
+                    </div>
+                  <p className="mt-2 text-lg font-medium tracking-tight text-gray-950 max-lg:text-center">
+                    Powerful APIs
+                  </p>
+                  <p className="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
+                    Sit quis amet rutrum tellus ullamcorper ultricies libero dolor eget sem sodales gravida.
+                  </p>
+                </div>
+               
+              </div>
+              <div className="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5 "></div>
+            </div>
+
+            <div className="relative lg:row-span-2">
+              <div className="absolute inset-px rounded-md bg-white "></div>
+              <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)] max-lg:rounded-b-[calc(2rem+1px)] lg:rounded-r-[calc(2rem+1px)]">
+                <div className="px-8 pb-3 pt-8 sm:px-10 sm:pb-0 sm:pt-10">
+                <div className='bg-red-300 pt-7 w-20 block mx-auto h-20 rounded-full'>
+                    <Timer className='mx-auto content-center'/>
+                    </div>
+                  <p className="mt-2 text-lg font-medium tracking-tight text-gray-950 max-lg:text-center">
+                    Powerful APIs
+                  </p>
+                  <p className="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
+                    Sit quis amet rutrum tellus ullamcorper ultricies libero dolor eget sem sodales gravida.
+                  </p>
+                </div>
+               
+              </div>
+              <div className="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5 max-lg:rounded-b-[2rem] lg:rounded-r-[2rem]"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+  
